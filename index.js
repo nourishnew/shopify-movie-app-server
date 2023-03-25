8f27a69e67de1207f7f405f3da11024ccef74f12
Nourish Cherish <ncherish@uwaterloo.ca>
2023-03-24 21:26:13 -0400

    server

--- index.js
+++ index.js
@@ -2,37 +2,40 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
+const PW = process.env.PW;
+
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({ limit: "500mb" }));
 
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 app.use(cors());
 app.get("/", (req, res) => {
-	res.send("shopify movies server connected");
+  res.send("shopify movies server connected");
 });
 mongoose
-	.connect(process.env.CONNECTION_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		app.listen(PORT, function () {
-			console.log("Listening on port " + PORT);
-		});
-	})
-	.catch((err) => {
-		console.log(err.message);
-	});
+  .connect(
+    `mongodb+srv://nourish:${PW}@cluster0.cnbun.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    app.listen(PORT, function () {
+      console.log("Listening on port " + PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 app.use("/app", require("./routes/server"));
 
 mongoose.set("useFindAndModify", false);
